ccf75b303d8111351e9fd5e609fbe07584470aee
Hein Htet Zaw <heinhtetzaw@viabells.com>
2024-06-26 06:52:41 +0630

    add course service

---
+++ server/services/course.service.ts
@@ -0,0 +1,24 @@
+import { Response } from "express";
+import { CatchAsyncError } from "../middlewares/catchAsyncErrors";
+import CourseModel from "../models/course.modal";
+
+// create course
+export const createCourse = CatchAsyncError(
+  async (data: any, res: Response) => {
+    const course = await CourseModel.create(data);
+    res.status(201).json({
+      success: true,
+      course,
+    });
+  }
+);
+
+// Get All Courses
+export const getAllCoursesService = async (res: Response) => {
+  const courses = await CourseModel.find().sort({ createdAt: -1 });
+
+  res.status(201).json({
+    success: true,
+    courses,
+  });
+};
