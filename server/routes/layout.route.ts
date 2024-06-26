c5f1a603da7d251f6a7f56a1ce038f30b3cfd742
Hein Htet Zaw <heinhtetzaw@viabells.com>
2024-06-26 22:33:28 +0630

    add layout service

---
+++ server/routes/layout.route.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import {
+  createLayout,
+  editLayout,
+  getLayoutByType,
+} from "../controllers/layout.controller";
+import { authorizeRoles, isAutheticated } from "../middlewares/auth";
+const layoutRouter = express.Router();
+
+layoutRouter.post(
+  "/create-layout",
+  isAutheticated,
+  authorizeRoles("admin"),
+  createLayout
+);
+
+layoutRouter.put(
+  "/edit-layout",
+  isAutheticated,
+  authorizeRoles("admin"),
+  editLayout
+);
+
+layoutRouter.get("/get-layout/:type", getLayoutByType);
+
+export default layoutRouter;
