53260df1cb2a2d27eb870dc24dccc7f51cd71c00
Hein Htet Zaw <heinh9540@gmail.com>
2024-06-26 22:45:34 +0630

    analytics service add

---
+++ server/routes/analytics.route.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import {
+  getCoursesAnalytics,
+  getOrderAnalytics,
+  getUsersAnalytics,
+} from "../controllers/analytics.controller";
+import { authorizeRoles, isAutheticated } from "../middlewares/auth";
+const analyticsRouter = express.Router();
+
+analyticsRouter.get(
+  "/get-users-analytics",
+  isAutheticated,
+  authorizeRoles("admin"),
+  getUsersAnalytics
+);
+
+analyticsRouter.get(
+  "/get-orders-analytics",
+  isAutheticated,
+  authorizeRoles("admin"),
+  getOrderAnalytics
+);
+
+analyticsRouter.get(
+  "/get-courses-analytics",
+  isAutheticated,
+  authorizeRoles("admin"),
+  getCoursesAnalytics
+);
+
+export default analyticsRouter;
